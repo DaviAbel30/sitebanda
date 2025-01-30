9dd832c3ee9c73acf93ecfcfb82c85388b2a9291
Davi Perucci <148788050+DaviAbel30@users.noreply.github.com>
2025-01-30 12:53:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+// Seleciona o botão hamburger, o menu e os links do menu
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
+const navLinks = document.querySelectorAll(".nav-list a"); // Seleciona os links dentro da lista do menu
 
-hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+// Alternar o menu ao clicar no botão hamburger
+hamburger.addEventListener("click", () => {
+      nav.classList.toggle("active");
+});
+
+// Fechar o menu ao clicar em qualquer link
+navLinks.forEach(link => {
+      link.addEventListener("click", () => {
+        console.log("Link clicado"); // Verifica se o clique no link está sendo detectado
+        if (nav.classList.contains("active")) {
+          nav.classList.remove("active");
+        }
+      });
+    });
+    
